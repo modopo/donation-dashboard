ab8fb0c0607963f3bd8bf3387d73273872a43d65
modopo <modopo@gmail.com>
2024-04-03 17:22:08 -0700

    renamed routes and added distribution route

--- server/routes/distributions.js
+++ server/routes/distributions.js
@@ -1,5 +1,26 @@
 import express from "express";
+import {
+  getStaffRecordedDistributions,
+  getAllDistributions,
+  addDistribution,
+  updateDistribution,
+  deleteDistribution,
+} from "../controllers/distribution.js";
+import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
+
+//READ
+router.get("/:id/distribution", verifyToken, getStaffRecordedDistributions);
+router.get("/:id/distributionList", verifyToken, getAllDistributions);
+
+//POST
+router.post("/:id/addDistribution", verifyToken, addDistribution);
+
+//UPDATE
+router.patch("/:id/:distributionId/update", verifyToken, updateDistribution);
+
+//DELET
+router.patch("/:id/:distributionId/delete", verifyToken, deleteDistribution);
 
 export default router;
