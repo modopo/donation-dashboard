eb6a6950714707fceb8ae0b045f73795245e6222
modopo <modopo@gmail.com>
2024-04-07 01:04:01 -0700

    get bank info for general glance view

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,5 +1,18 @@
 import express from "express";
+import {
+  getAllBanks,
+  getAllFood,
+  getAllItems,
+  getAllMoney,
+} from "../controllers/general.js";
+import { verifyToken } from "../middleware/auth";
 
 const router = express.Router();
+
+//READ
+router.get("/", verifyToken, getAllBanks);
+router.get("/food", verifyToken, getAllFood);
+router.get("/items", verifyToken, getAllItems);
+router.get("/money", verifyToken, getAllMoney);
 
 export default router;
