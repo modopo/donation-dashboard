ea87c601bf984caf2f70417b95a21023a784edea
modopo <modopo@gmail.com>
2024-05-03 18:15:56 -0700

    get user in RTK

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -13,14 +13,6 @@ export const globalSlice = createSlice({
     setMode: (state) => {
       state.mode = state.mode === "light" ? "dark" : "light";
     },
-    setLogin: (state, action) => {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-    },
-    setLogout: (state) => {
-      state.user = null;
-      state.token = null;
-    },
   },
 });
 
