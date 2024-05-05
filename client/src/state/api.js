e23c9509b33614224d74d7af92c3c68dbb60a31d
modopo <modopo@gmail.com>
2024-05-04 20:16:55 -0700

    login and register mutation in API in RTK

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,8 +3,24 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: ["User"],
+  tagTypes: ["User", "Register", "Login"],
   endpoints: (build) => ({
+    registerUser: build.mutation({
+      query: (userData) => ({
+        url: "auth/register",
+        method: "POST",
+        body: userData,
+      }),
+      providesTags: ["Register"],
+    }),
+    loginUser: build.mutation({
+      query: (loginData) => ({
+        url: "auth/login",
+        method: "POST",
+        body: loginData,
+      }),
+      providesTags: ["Login"],
+    }),
     getUser: build.query({
       query: (id) => `user/${id}`,
       providesTags: ["User"],
@@ -12,4 +28,8 @@ export const api = createApi({
   }),
 });
 
-export const { useGetUserQuery } = api;
+export const {
+  useGetUserQuery,
+  useLoginUserMutation,
+  useRegisterUserMutation,
+} = api;
