5bc53015fc5882706bc43b68ea8af2ebe889a5e1
modopo <modopo@gmail.com>
2024-05-15 13:45:16 -0700

    layout of sidebar with routes

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -3,7 +3,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
-// import Sidebar from "components/Sidebar";
+import Sidebar from "components/Sidebar";
 import { useGetUserQuery } from "state/api";
 
 const Layout = () => {
@@ -14,13 +14,13 @@ const Layout = () => {
 
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
-      {/* <Sidebar */}
-      {/*   user={data || {}} */}
-      {/*   isNonMobile={isNonMobile} */}
-      {/*   drawerWidth="250px" */}
-      {/*   isSidebarOpen={isSidebarOpen} */}
-      {/*   setIsSidebarOpen={setIsSidebarOpen} */}
-      {/* /> */}
+      <Sidebar
+        user={data || {}}
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box flexGrow={1}>
         <Navbar
           user={data || {}}
