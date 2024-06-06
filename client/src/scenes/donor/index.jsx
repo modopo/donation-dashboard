a54f05f6d9ec660b8ea8c925d017f15a22d580d9
modopo <modopo@gmail.com>
2024-06-05 17:37:39 -0700

    donor react template

---
+++ client/src/scenes/donor/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { useGetDonorQuery } from "../../state/api";
+
+const Donors = () => {
+  return <div>Donors</div>;
+};
+
+export default Donors;
